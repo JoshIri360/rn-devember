bf95b1ff9d43f8bd9cc55f34cb12b991023c48cb
Joshua Aideloje <aidelojejoshua@gmail.com>
2024-02-02 17:12:54 +0100

    👌 IMPROVE: Update fonts and SplashScreen usage in _layout.tsx and index.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,26 @@
-import { Stack } from "expo-router";
-import index from ".";
+import { SplashScreen, Stack } from "expo-router";
+import { Montserrat_700Bold, useFonts } from "@expo-google-fonts/montserrat";
+import { Inika_400Regular } from "@expo-google-fonts/inika";
+import { useCallback } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded, fontError] = useFonts({
+    MontserratBold: Montserrat_700Bold,
+    InikaRegular: Inika_400Regular,
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <Stack screenOptions={{}}>
       <Stack.Screen name="index" options={{ headerShown: false }} />
--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,5 @@
-import {
-  Dimensions,
-  FlatList,
-  ListRenderItem,
-  FlatList as RNFlatList,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import { Dimensions, FlatList, SafeAreaView, StyleSheet } from "react-native";
 import React, { useCallback } from "react";
-import { SplashScreen } from "expo-router";
-import { useFonts } from "expo-font";
-import { Montserrat_700Bold } from "@expo-google-fonts/montserrat";
-import { Inika_400Regular } from "@expo-google-fonts/inika";
 import DayListItem from "@/components/core/DayListItem";
 import HomePageFeatureCard from "@/components/core/HomePageFeatureCard";
 import { NativeWindStyleSheet } from "nativewind";
@@ -21,24 +8,7 @@ NativeWindStyleSheet.setOutput({
   default: "native",
 });
 
-SplashScreen.preventAutoHideAsync();
-
 export default function index() {
-  const [fontsLoaded, fontError] = useFonts({
-    MontserratBold: Montserrat_700Bold,
-    InikaRegular: Inika_400Regular,
-  });
-
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded || fontError) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, fontError]);
-
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
-
   const days = Array.from({ length: 31 }, (_, i) => i + 1);
 
   const itemWidth = 104;
@@ -48,13 +18,10 @@ export default function index() {
   const numColumns = Math.floor(screenWidth / (itemWidth + gap));
 
   return (
-    <SafeAreaView
-      className="flex-1 items-center justify-center bg-darkBackground"
-      onLayout={onLayoutRootView}
-    >
+    <SafeAreaView className="flex-1 items-center justify-center bg-darkBackground">
       <HomePageFeatureCard />
       <FlatList
-        contentContainerStyle={{ gap: gap, padding: gap}}
+        contentContainerStyle={{ gap: gap, padding: gap }}
         columnWrapperStyle={{ gap: gap }}
         showsVerticalScrollIndicator={false}
         numColumns={numColumns}
