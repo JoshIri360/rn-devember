17581b8d5f31b2ae190d2ea5eec44735eb2714d4
Joshua Aideloje <aidelojejoshua@gmail.com>
2024-02-02 10:33:01 +0100

    📦 NEW: Add expo-font and custom fonts

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,32 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useCallback } from "react";
+import { SplashScreen } from "expo-router";
+import { useFonts } from "expo-font";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function index() {
+  const [fontsLoaded, fontError] = useFonts({
+    "Inika-Bold": require("./assets/fonts/Inika-Bold.ttf"),
+    "Inika-Regular": require("./assets/fonts/Inika-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
   return (
-    <View className="flex-1 bg-white items-center justify-center">
-      <View style={styles.day}>
-        <Text>1</Text>
+    <View
+      className="flex-1 items-center justify-center gap-3 bg-darkBackground"
+      onLayout={onLayoutRootView}
+    >
+      <View className="bg-chocolateBrown w-32 h-32 items-center justify-center rounded-xl">
+        <Text className="text-5xl text-lightText">1</Text>
       </View>
-      <View style={styles.day}>
-        <Text>2</Text>
+      <View className="bg-chocolateBrown w-32 h-32 items-center justify-center rounded-xl">
+        <Text className="text-5xl text-lightText">2</Text>
       </View>
     </View>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
     extend: {
       colors: {
         chocolateBrown: "#AB5E2A",
+        darkBackground: "#16282F",
+        lightText: "#FCE9E2"
       },
     },
   },
